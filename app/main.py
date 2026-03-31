de9626fe7dc3bf712da978aff2976a818ee6b8ee
amesemyta1 <amesemyta@gmail.com>
2026-03-31 16:26:59 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,47 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int | float) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: int | float | "Distance") -> Distance:
+        return Distance(self.km + self.get_value(other))
+
+    def __iadd__(self, other: int | float | "Distance") -> Distance:
+        self.km += self.get_value(other)
+        return self
+
+    def __mul__(self, other: int | float) -> Distance:
+        if isinstance(other, Distance):
+            raise TypeError
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: int | float) -> Distance:
+        if isinstance(other, Distance):
+            raise TypeError
+        return Distance(round(self.km / other, 2))
+
+    def __eq__(self, other: int | float | "Distance") -> bool:
+        return self.km == self.get_value(other)
+
+    def __lt__(self, other: int | float | "Distance") -> bool:
+        return self.km < self.get_value(other)
+
+    def __gt__(self, other: int | float | "Distance") -> bool:
+        return self.km > self.get_value(other)
+
+    def __le__(self, other: int | float | "Distance") -> bool:
+        return self.km <= self.get_value(other)
+
+    def __ge__(self, other: int | float | "Distance") -> bool:
+        return self.km >= self.get_value(other)
+
+    @staticmethod
+    def get_value(other: int | float | Distance) -> int | float:
+        if isinstance(other, Distance):
+            return other.km
+        return other
